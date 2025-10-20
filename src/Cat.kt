45c255e792a738b8961e125b181fef10e31af72f
Luiz <purez@Luiz-93Pureza.>
2025-10-20 22:23:19 +0100

    adding the 'distance' parameter to dev branch

--- src/Cat.kt
+++ src/Cat.kt
@@ -3,7 +3,7 @@ class Cat (val name: String, var age: Int, var weight: Int) {
     // Reduces weight by one
     // Should check if the weight is 5 or more
     // There is a bug - don't resolve until instructed by the exercise.
-    fun walk() :Boolean {
+    fun walk(distance: Int) :Boolean {
         if(weight <= 5) {
             weight--
             return true
